502a626b4b135fed69f3d8eeb5b96ac3fb025823
Evoluti83 <bauduin.teddy62@gmail.com>
2026-04-03 12:47:11 +0200

    fix: chemins absolus index.php pour Heroku

--- public/index.php
+++ public/index.php
@@ -7,28 +7,30 @@ require_once __DIR__ . '/../src/config/mail.php';
 
 $route = $_GET['page'] ?? 'accueil';
 
+$base = __DIR__ . '/../src/controllers/';
+
 $routes = [
-    'accueil'        => '../src/controllers/AccueilController.php',
-    'menus'          => '../src/controllers/MenusController.php',
-    'menu-detail'    => '../src/controllers/MenuDetailController.php',
-    'contact'        => '../src/controllers/ContactController.php',
-    'inscription'    => '../src/controllers/InscriptionController.php',
-    'connexion'      => '../src/controllers/ConnexionController.php',
-    'deconnexion'    => '../src/controllers/DeconnexionController.php',
-    'mot-de-passe'   => '../src/controllers/MotDePasseController.php',
-    'commande'       => '../src/controllers/CommandeController.php',
-    'espace-user'    => '../src/controllers/EspaceUserController.php',
-    'espace-employe' => '../src/controllers/EspaceEmployeController.php',
-    'espace-admin'   => '../src/controllers/EspaceAdminController.php',
-    'mentions'       => '../src/controllers/MentionsController.php',
-    'cgv'            => '../src/controllers/CgvController.php',
+    'accueil'        => $base . 'AccueilController.php',
+    'menus'          => $base . 'MenusController.php',
+    'menu-detail'    => $base . 'MenuDetailController.php',
+    'contact'        => $base . 'ContactController.php',
+    'inscription'    => $base . 'InscriptionController.php',
+    'connexion'      => $base . 'ConnexionController.php',
+    'deconnexion'    => $base . 'DeconnexionController.php',
+    'mot-de-passe'   => $base . 'MotDePasseController.php',
+    'commande'       => $base . 'CommandeController.php',
+    'espace-user'    => $base . 'EspaceUserController.php',
+    'espace-employe' => $base . 'EspaceEmployeController.php',
+    'espace-admin'   => $base . 'EspaceAdminController.php',
+    'mentions'       => $base . 'MentionsController.php',
+    'cgv'            => $base . 'CgvController.php',
 ];
 
 if (array_key_exists($route, $routes)) {
     require_once $routes[$route];
 } else {
     http_response_code(404);
-    require_once '../src/controllers/NotFoundController.php';
+    echo '<h1>Page non trouvée</h1>';
 }
 
 ob_end_flush();
