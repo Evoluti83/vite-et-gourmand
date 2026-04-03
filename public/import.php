137827520f99fda3cef26ac2411f1cc54aa4b55b
Evoluti83 <bauduin.teddy62@gmail.com>
2026-04-03 12:59:00 +0200

    temp: import images BDD

--- public/import.php
+++ public/import.php
@@ -5,21 +5,27 @@ require_once __DIR__ . '/../src/config/db.php';
 try {
     $pdo = getDB();
     
-    $sql = file_get_contents(__DIR__ . '/../database/create.sql');
-    $pdo->exec($sql);
-    echo "create.sql importé !<br>";
+    // Insertion des images
+    $pdo->exec("DELETE FROM image_menu");
+    $pdo->exec("
+        INSERT INTO image_menu (image_id, menu_id, chemin, ordre) VALUES
+        (6, 1, 'assets/images/menus/menu_1_1775206297_0.jpg', 3),
+        (7, 1, 'assets/images/menus/menu_1_1775206384_0.jpg', 4),
+        (8, 2, 'assets/images/menus/menu_2_1775206459_0.jpg', 2),
+        (9, 2, 'assets/images/menus/menu_2_1775206469_0.jpg', 3),
+        (10, 3, 'assets/images/menus/menu_3_1775206483_0.jpg', 1),
+        (11, 3, 'assets/images/menus/menu_3_1775206490_0.jpg', 2),
+        (12, 4, 'assets/images/menus/menu_4_1775206559_0.jpg', 1),
+        (13, 4, 'assets/images/menus/menu_4_1775206565_0.jpg', 2)
+    ");
+    echo "Images importées avec succès !";
     
-    $sql2 = file_get_contents(__DIR__ . '/../database/insert.sql');
-    $pdo->exec($sql2);
-    echo "insert.sql importé !<br>";
-    
-    echo "<br><strong>Base de données créée avec succès !</strong>";
+    // Mise à jour des mots de passe
+    $hash = password_hash('Password1!', PASSWORD_BCRYPT);
+    $pdo->prepare("UPDATE utilisateur SET password = :hash WHERE email IN ('client@test.fr', 'julie@viteetgourmand.fr', 'jose@viteetgourmand.fr')")
+        ->execute(['hash' => $hash]);
+    echo "<br>Mots de passe mis à jour !";
+
 } catch (Exception $e) {
     echo "Erreur : " . $e->getMessage();
 }
-
-// Mise à jour des mots de passe
-$hash = password_hash('Password1!', PASSWORD_BCRYPT);
-$pdo->prepare("UPDATE utilisateur SET password = :hash WHERE email IN ('client@test.fr', 'julie@viteetgourmand.fr', 'jose@viteetgourmand.fr')")
-    ->execute(['hash' => $hash]);
-echo "<br>Mots de passe mis à jour !";
